00010b58020388a1a102d94f56e44c6e15569e96
Yung Dai <yungchidai@gmail.com>
2015-04-08 14:39:58 -0400

    push code that I have typed in of the lesson

--- Assignment4/main.m
+++ Assignment4/main.m
@@ -8,10 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+
+int getRandomInteger(int minimum, int maximum) {
+    return arc4random_uniform((maximum - minimum) + 1) + minimum;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int randomNumber = getRandomInteger(-10, 10);
+        NSLog(@"Selected a random number between -10 and 10: %d", randomNumber);
     }
     return 0;
 }
