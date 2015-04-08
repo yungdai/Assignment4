c15854948e4eb859a43726a9233042a0d4bb5dba
Yung Dai <yungchidai@gmail.com>
2015-04-08 15:43:31 -0400

    Static Local Variables

---
+++ Assignment4/StaticLocalVariables.m
@@ -0,0 +1,24 @@
+//
+//  StaticLocalVariables.m
+//  Assignment4
+//
+//  Created by Yung Dai on 2015-04-08.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int countByTwo() {
+    static int currentCount = 0;
+    currentCount += 2;
+    return currentCount;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"%d", countByTwo());    // 2
+        NSLog(@"%d", countByTwo());    // 4
+        NSLog(@"%d", countByTwo());    // 6
+    }
+    return 0;
+}
