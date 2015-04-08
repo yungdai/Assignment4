300320bc2b462ee53271c1fb2b3ca99ec8ad24ae
Yung Dai <yungchidai@gmail.com>
2015-04-08 15:50:24 -0400

    Function Libraries 2

--- Assignment4/main.m
+++ Assignment4/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CarUtilities.h"
 
 
 // get a random number between -10 and 10
@@ -15,10 +16,16 @@ int getRandomInteger(int minimum, int maximum) {
     return arc4random_uniform((maximum - minimum) + 1) + minimum;
 }
 
-int main1(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int randomNumber = getRandomInteger(-10, 10);
-        NSLog(@"Selected a random number between -10 and 10: %d", randomNumber);
+        NSDictionary *makesAndModels = @{
+                                         @"Ford": @[@"Explorer", @"F-150"],
+                                         @"Honda": @[@"Accord", @"Civic", @"Pilot"],
+                                         @"Nissan": @[@"370Z", @"Altima", @"Versa"],
+                                         @"Porsche": @[@"911 Turbo", @"Boxster", @"Cayman S"]
+                                         };
+        NSString *randomCar = CUGetRandomMakeAndModel(makesAndModels);
+        NSLog(@"Selected a %@", randomCar);
     }
     return 0;
 }
